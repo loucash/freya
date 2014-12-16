32273fe83c42f90e1baded54d28a7f138bf47b4c
Łukasz Biedrycki <lukasz.biedrycki@gmail.com>
2014-12-16 21:01:44 +0000

    Make encode long value compatible with kairos

--- src/freya_utils.erl
+++ src/freya_utils.erl
@@ -3,6 +3,7 @@
 -include("freya.hrl").
 
 -export([floor/2, ceil/2, prev/2, next/2, ms/1]).
+-export([pack_long/1, unpack_long/1]).
 
 -spec floor(milliseconds(), precision()) ->
     milliseconds().
@@ -31,3 +32,10 @@ ms({N, days}) ->
     N * timer:hours(24);
 ms({N, Tp}) ->
     timer:Tp(N).
+
+% kairos uses it: https://developers.google.com/protocol-buffers/docs/encoding?csw=1#types
+unpack_long(Value) when is_integer(Value) ->
+    (Value bsr 1) bxor -(Value band 1).
+
+pack_long(Value) when is_integer(Value) ->
+    (Value bsl 1) bxor  (Value bsr 63).
