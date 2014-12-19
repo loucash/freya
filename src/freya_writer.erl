63f380e865c5f3fe20f41166a2007d59306c85cc
Łukasz Biedrycki <lukasz.biedrycki@gmail.com>
2014-12-19 12:06:32 +0100

    Reorganize code

--- src/freya_writer.erl
+++ src/freya_writer.erl
@@ -5,6 +5,8 @@
 
 %% API
 -export([start_link/1]).
+-export([statements/0]).
+-export([save/2]).
 
 %% gen_server callbacks
 -export([init/1,
@@ -23,6 +25,25 @@
 %%%===================================================================
 %%% API
 %%%===================================================================
+statements() ->
+    [
+     {?INSERT_DATA_POINT_TTL,
+      <<"INSERT INTO data_points (key, column1, value) "
+        "VALUES (?, ?, ?) USING TTL ?;">>},
+     {?INSERT_DATA_POINT,
+      <<"INSERT INTO data_points (key, column1, value) "
+        "VALUES (?, ?, ?);">>},
+     {?INSERT_ROW_INDEX,
+      <<"INSERT INTO row_key_index (key, column1, value) "
+        "VALUES (?, ?, ?);">>},
+     {?INSERT_STRING_INDEX,
+      <<"INSERT INTO string_index (key, column1, value) "
+        "VALUES (?, ?, ?);">>}
+    ].
+
+save(Publisher, #data_point{}=DP) ->
+    Qrys = save_data_point_queries(DP),
+    eqm_pub:post(Publisher, Qrys).
 
 start_link(Args) ->
     gen_server:start_link(?MODULE, Args, []).
@@ -90,3 +111,14 @@ code_change(_OldVsn, State, _Extra) ->
 consistency() ->
     A = freya:get_env(cassandra_consistency, quorum),
     {consistency, A}.
+
+save_data_point_queries(#data_point{}=DP) ->
+    {ok, {Row, Column, Value}} = freya_blobs:encode(DP),
+    [{?INSERT_DATA_POINT, [Row, Column, Value]},
+     {?INSERT_ROW_INDEX, [DP#data_point.name, Row, <<0>>]},
+     {?INSERT_STRING_INDEX, [?ROW_KEY_METRIC_NAMES, DP#data_point.name, <<0>>]}]
+    ++ lists:flatmap(
+         fun(TagName, TagValue) ->
+            [{?INSERT_STRING_INDEX, [?ROW_KEY_TAG_NAMES, TagName, <<0>>]},
+             {?INSERT_STRING_INDEX, [?ROW_KEY_TAG_VALUES, TagValue, <<0>>]}]
+         end, DP#data_point.tags).
