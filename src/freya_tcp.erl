57eac17209fe8908a16fc9d01e6bbdd5349fa628
Adam Rutkowski <hq@mtod.org>
2015-01-07 12:59:14 +0100

    Dispatch TCP to protocol handler

--- src/freya_tcp.erl
+++ src/freya_tcp.erl
@@ -2,42 +2,48 @@
 -behaviour(ranch_protocol).
 
 % api
--export([start/0]).
+-export([start/1]).
 
 % ranch callbacks
 -export([start_link/4]).
 -export([init/4]).
 
+-include("freya_tcp.hrl").
 
--define(DEFAULT_PORT, 7055).
--define(DEFAULT_CODEC, freya_tcp_codec).
-
--record(state, {codec = ?DEFAULT_CODEC :: module()}).
-
-start() ->
+-spec start(eqm:pub()) -> ok.
+start(Publisher) ->
+    {ok, Version} = freya:version(),
     Port = freya:get_env(w_tcp_port, ?DEFAULT_PORT),
-    {ok, _} = ranch:start_listener(w_tcp, 100, ranch_tcp,
-                                   [{port, Port}], ?MODULE, []),
+    Opts = [{publisher, Publisher}, {version, Version}],
+
+    lager:info("Starting TCP freya interface ~s / ~p", [Version, Publisher]),
+    {ok, _} = ranch:start_listener(freya_tcp_w, 100, ranch_tcp,
+                                   [{port, Port}], ?MODULE, Opts),
     ok.
+
 
 start_link(Ref, Socket, Transport, Opts) ->
     Pid = spawn_link(?MODULE, init, [Ref, Socket, Transport, Opts]),
     {ok, Pid}.
 
-init(Ref, Socket, Transport, _Opts=[]) ->
+init(Ref, Socket, Transport, Opts) ->
     ok = ranch:accept_ack(Ref),
-    loop(Socket, Transport, #state{}).
+    Pub = kvlists:get_value(publisher, Opts),
+    Vsn = kvlists:get_value(version, Opts),
+    loop(Socket, Transport, #proto{publisher=Pub, version=Vsn}).
 
 loop(Socket, Transport, State) ->
     case Transport:recv(Socket, 0, 5000) of
         {ok, Data} ->
             case handle_raw_data(Data, State) of
-                {ok, MaybeNewState} ->
-                    Ack = (State#state.codec):encode(ack),
-                    Transport:send(Socket, Ack),
+                {noreply, MaybeNewState} ->
                     loop(Socket, Transport, MaybeNewState);
-                {error, _Reason} ->
-                    % TODO add lager
+                {reply, Reply, MaybeNewState} ->
+                    Ack = (State#proto.codec):encode(Reply),
+                    ok = Transport:send(Socket, Ack),
+                    loop(Socket, Transport, MaybeNewState);
+                {error, Reason} ->
+                    lager:error("Freya protocol error: ~p", [Reason]),
                     ok = Transport:close(Socket)
             end;
         _ ->
@@ -45,9 +51,5 @@ loop(Socket, Transport, State) ->
     end.
 
 handle_raw_data(RawData, State) ->
-    Data = (State#state.codec):decode(RawData),
-    handle_data(Data, State).
-
-handle_data(Data, State) ->
-    io:format("Received ~p~n", [Data]),
-    {ok, State}.
+    Data = (State#proto.codec):decode(RawData),
+    freya_tcp_proto:inbound(Data, State).
