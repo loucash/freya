c018bc006040cfbb52f7ff8f0e90713df9f9b9c0
Adam Rutkowski <hq@mtod.org>
2015-01-07 12:56:36 +0100

    Add freya:version/0 interface

--- src/freya.erl
+++ src/freya.erl
@@ -1,6 +1,7 @@
 -module(freya).
 -export([start/0, stop/0]).
 -export([get_env/1, get_env/2]).
+-export([version/0]).
 
 -define(APP, ?MODULE).
 
@@ -15,3 +16,11 @@ get_env(Name) ->
 
 get_env(Name, Default) ->
     application:get_env(?APP, Name, Default).
+
+version() ->
+    {ok, Vsn} = application:get_key(freya, vsn),
+    CompileTime = kvlists:get_path([compile, time], freya_app:module_info()),
+    {Y,M,D,Hr,Min,Sec} = CompileTime,
+    BuildDate = tic:datetime_to_iso8601({{Y,M,D},{Hr,Min,Sec}}),
+    Version = [atom_to_list(?MODULE), " ", Vsn, " build: ", BuildDate],
+    {ok, Version}.
