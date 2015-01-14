6a0b5591c05452e383acaa35042e0208726914c0
Adam Rutkowski <hq@mtod.org>
2015-01-14 14:39:59 +0100

    Define TCP records and macros

--- src/freya_tcp.hrl
+++ src/freya_tcp.hrl
@@ -1,6 +1,15 @@
 -define(DEFAULT_PORT, 7055).
 -define(DEFAULT_CODEC, freya_tcp_codec).
 
+-define(HDR            , 16#FF).
+-define(OPCODE_VERSION , $v).
+-define(OPCODE_PUT     , $p).
+-define(OPCODE_STATUS  , $s).
+
+
 -record(proto, { codec = ?DEFAULT_CODEC :: module(),
-                 publisher = eqm:pub(),
-                 version :: iodata() }).
+                 socket                 :: inet:socket(),
+                 transport              :: any(),
+                 publisher              :: eqm:pub(),
+                 version                :: iodata() 
+               }).
