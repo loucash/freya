f213568cfe6f4a86d3964e0d2a0aef5aca9e813a
Adam Rutkowski <hq@mtod.org>
2015-01-14 15:16:34 +0100

    Add testcase for writing metric via tcp

--- test/freya_io_SUITE.erl
+++ test/freya_io_SUITE.erl
@@ -14,7 +14,8 @@
          t_avg_aggregate_aligned/1,
          t_start_end_time/1,
          t_start_end_time_different_rowkeys/1,
-         t_tcp_version/1]).
+         t_tcp_version/1,
+         t_tcp_write/1]).
 
 -include("freya.hrl").
 -include_lib("common_test/include/ct.hrl").
@@ -39,7 +40,8 @@ all() ->
      t_sum_aggregate_aligned_different_types,
      t_avg_aggregate,
      t_avg_aggregate_aligned,
-     t_tcp_version
+     t_tcp_version,
+     t_tcp_write
     ].
 
 init_per_suite(Config) ->
@@ -56,10 +58,11 @@ t_write_read_data_point(_Config) ->
     Ts = tic:now_to_epoch_msecs(),
     DPIn = freya_data_point:new(MetricName, Ts, <<"kairos_long">>, 1),
     ok = freya_writer:save(Publisher, DPIn),
-    timer:sleep(2000),
-    {ok, [DPIn]} = freya_reader:search(?CS_READ_POOL, [{metric_name, MetricName},
-                                                       {start_time, Ts}]),
-    ok.
+    ?th:keep_trying({ok, [DPIn]}, fun() ->
+                                          freya_reader:search(?CS_READ_POOL,
+                                                              [{metric_name, MetricName},
+                                                               {start_time, Ts}])
+                                  end, 100, 200).
 
 t_write_read_different_rows(_Config) ->
     {ok, Publisher} = eqm:publisher_info(?CS_WRITERS_PUB),
@@ -70,11 +73,10 @@ t_write_read_different_rows(_Config) ->
     DPIn2 = freya_data_point:new(MetricName, Ts2, <<"kairos_long">>, 1),
     ok = freya_writer:save(Publisher, DPIn1),
     ok = freya_writer:save(Publisher, DPIn2),
-    timer:sleep(2000),
-    {ok, [DPIn1, DPIn2]} = freya_reader:search(?CS_READ_POOL,
+    ?th:keep_trying({ok, [DPIn1, DPIn2]}, fun() ->freya_reader:search(?CS_READ_POOL,
                                                [{metric_name, MetricName},
-                                                {start_time, Ts1}]),
-    ok.
+                                                {start_time, Ts1}])
+                                          end, 100, 200).
 
 t_read_row_size(_Config) ->
     meck:new(freya_reader, [passthrough]),
@@ -90,12 +92,13 @@ t_read_row_size(_Config) ->
     ok = freya_writer:save(Publisher, DPIn2),
     ok = freya_writer:save(Publisher, DPIn3),
     ok = freya_writer:save(Publisher, DPIn4),
-    timer:sleep(2500),
-    {ok, [DPIn1, DPIn2, DPIn3, DPIn4]} = freya_reader:search(?CS_READ_POOL,
-                                                             [{metric_name, MetricName},
-                                                              {start_time, Ts}]),
-    meck:unload(),
-    ok.
+    ?th:keep_trying({ok, [DPIn1, DPIn2, DPIn3, DPIn4]},
+                    fun() ->
+                            freya_reader:search(?CS_READ_POOL,
+                                                [{metric_name, MetricName},
+                                                 {start_time, Ts}])
+                    end, 100, 200),
+    meck:unload().
 
 t_read_row_size_and_desc(_Config) ->
     meck:new(freya_reader, [passthrough]),
@@ -111,11 +114,12 @@ t_read_row_size_and_desc(_Config) ->
     ok = freya_writer:save(Publisher, DPIn2),
     ok = freya_writer:save(Publisher, DPIn3),
     ok = freya_writer:save(Publisher, DPIn4),
-    timer:sleep(2500),
-    {ok, [DPIn4, DPIn3, DPIn2, DPIn1]} = freya_reader:search(?CS_READ_POOL,
-                                                             [{metric_name, MetricName},
-                                                              {start_time, Ts},
-                                                              {order, desc}]),
+    ?th:keep_trying({ok, [DPIn4, DPIn3, DPIn2, DPIn1]},
+                    fun() -> freya_reader:search(?CS_READ_POOL,
+                                                 [{metric_name, MetricName},
+                                                  {start_time, Ts},
+                                                  {order, desc}])
+                    end, 100, 200),
     meck:unload(),
     ok.
 
@@ -131,12 +135,12 @@ t_start_end_time(_Config) ->
     ok = freya_writer:save(Publisher, DPIn2),
     ok = freya_writer:save(Publisher, DPIn3),
     ok = freya_writer:save(Publisher, DPIn4),
-    timer:sleep(2500),
-    {ok, [DPIn1, DPIn2]} = freya_reader:search(?CS_READ_POOL,
-                                               [{metric_name, MetricName},
-                                                {start_time, Ts},
-                                                {end_time, Ts+1}]),
-    ok.
+    ?th:keep_trying({ok, [DPIn1, DPIn2]},
+                    fun() ->freya_reader:search(?CS_READ_POOL,
+                                                [{metric_name, MetricName},
+                                                 {start_time, Ts},
+                                                 {end_time, Ts+1}])
+                    end, 100, 200).
 
 t_start_end_time_different_rowkeys(_Config) ->
     {ok, Publisher} = eqm:publisher_info(?CS_WRITERS_PUB),
@@ -147,12 +151,12 @@ t_start_end_time_different_rowkeys(_Config) ->
     DPIn2 = freya_data_point:new(MetricName, Ts2, <<"kairos_long">>, 1),
     ok = freya_writer:save(Publisher, DPIn1),
     ok = freya_writer:save(Publisher, DPIn2),
-    timer:sleep(2500),
-    {ok, [DPIn1, DPIn2]} = freya_reader:search(?CS_READ_POOL,
-                                               [{metric_name, MetricName},
-                                                {start_time, Ts1},
-                                                {end_time, Ts2}]),
-    ok.
+    ?th:keep_trying({ok, [DPIn1, DPIn2]},
+                    fun() -> freya_reader:search(?CS_READ_POOL,
+                                                 [{metric_name, MetricName},
+                                                  {start_time, Ts1},
+                                                  {end_time, Ts2}])
+                    end, 100, 200).
 
 t_filter_tags_1(_Config) ->
     {ok, Publisher} = eqm:publisher_info(?CS_WRITERS_PUB),
@@ -164,12 +168,11 @@ t_filter_tags_1(_Config) ->
                                  [{<<"baz">>, <<"fox">>}]),
     ok = freya_writer:save(Publisher, DPIn1),
     ok = freya_writer:save(Publisher, DPIn2),
-    timer:sleep(2000),
-    {ok, [DPIn1]} = freya_reader:search(?CS_READ_POOL,
+    ?th:keep_trying({ok, [DPIn1]}, fun() ->freya_reader:search(?CS_READ_POOL,
                                         [{metric_name, MetricName},
                                          {start_time, Ts},
-                                         {tags, [{<<"foo">>, <<"bar">>}]}]),
-    ok.
+                                         {tags, [{<<"foo">>, <<"bar">>}]}])
+                                   end, 100, 200).
 
 t_filter_tags_2(_Config) ->
     {ok, Publisher} = eqm:publisher_info(?CS_WRITERS_PUB),
@@ -179,12 +182,12 @@ t_filter_tags_2(_Config) ->
                                 [{<<"foo">>, <<"bar">>},
                                  {<<"baz">>, <<"fox">>}]),
     ok = freya_writer:save(Publisher, DPIn),
-    timer:sleep(2000),
-    {ok, [DPIn]} = freya_reader:search(?CS_READ_POOL,
-                                       [{metric_name, MetricName},
-                                         {start_time, Ts},
-                                         {tags, [{<<"foo">>, <<"bar">>}]}]),
-    ok.
+    ?th:keep_trying({ok, [DPIn]},
+                    fun() -> freya_reader:search(?CS_READ_POOL,
+                                                 [{metric_name, MetricName},
+                                                  {start_time, Ts},
+                                                  {tags, [{<<"foo">>, <<"bar">>}]}])
+                    end, 100, 200).
 
 t_sum_aggregate(_Config) ->
     meck:new(freya_reader, [passthrough]),
@@ -200,15 +203,15 @@ t_sum_aggregate(_Config) ->
     ok = freya_writer:save(Publisher, DPIn2),
     ok = freya_writer:save(Publisher, DPIn3),
     ok = freya_writer:save(Publisher, DPIn4),
-    timer:sleep(2500),
     DPOut = DPIn1#data_point{value=4},
-    {ok, [DPOut]} =
-    freya_reader:search(?CS_READ_POOL,
-                        [{metric_name, MetricName},
-                         {start_time, Ts},
-                         {aggregate, {sum, {1, seconds}}}]),
-    meck:unload(),
-    ok.
+    ?th:keep_trying({ok, [DPOut]},
+                    fun() ->
+                            freya_reader:search(?CS_READ_POOL,
+                                                [{metric_name, MetricName},
+                                                 {start_time, Ts},
+                                                 {aggregate, {sum, {1, seconds}}}])
+                    end, 100, 200),
+    meck:unload().
 
 t_sum_aggregate_aligned(_Config) ->
     meck:new(freya_reader, [passthrough]),
@@ -224,16 +227,16 @@ t_sum_aggregate_aligned(_Config) ->
     ok = freya_writer:save(Publisher, DPIn2),
     ok = freya_writer:save(Publisher, DPIn3),
     ok = freya_writer:save(Publisher, DPIn4),
-    timer:sleep(2500),
     DPOut = DPIn1#data_point{value=4, ts=Ts},
-    {ok, [DPOut]} =
-    freya_reader:search(?CS_READ_POOL,
-                        [{metric_name, MetricName},
-                         {start_time, Ts},
-                         {aggregate, {sum, {1, seconds}}},
-                         {align, true}]),
-    meck:unload(),
-    ok.
+    ?th:keep_trying({ok, [DPOut]},
+                    fun() ->
+                            freya_reader:search(?CS_READ_POOL,
+                                                [{metric_name, MetricName},
+                                                 {start_time, Ts},
+                                                 {aggregate, {sum, {1, seconds}}},
+                                                 {align, true}])
+                    end, 100, 200),
+    meck:unload().
 
 t_sum_aggregate_aligned_different_types(_Config) ->
     meck:new(freya_reader, [passthrough]),
@@ -257,18 +260,20 @@ t_sum_aggregate_aligned_different_types(_Config) ->
     ok = freya_writer:save(Publisher, DPIn6),
     ok = freya_writer:save(Publisher, DPIn7),
     ok = freya_writer:save(Publisher, DPIn8),
-    timer:sleep(2500),
     DPOut1 = DPIn1#data_point{value=4, ts=Ts},
     DPOut2 = DPIn5#data_point{value=4.0, ts=Ts},
-    {ok, [DPOut3, DPOut4]} =
-    freya_reader:search(?CS_READ_POOL,
-                        [{metric_name, MetricName},
-                         {start_time, Ts},
-                         {aggregate, {sum, {1, seconds}}},
-                         {align, true}]),
-    meck:unload(),
-    true = sets:from_list([DPOut1, DPOut2]) =:= sets:from_list([DPOut3, DPOut4]),
-    ok.
+    Set = sets:from_list([DPOut1, DPOut2]),
+    ?th:keep_trying(Set,
+                    fun() ->
+                            {ok, L} = freya_reader:search(?CS_READ_POOL,
+                                                          [{metric_name, MetricName},
+                                                           {start_time, Ts},
+                                                           {aggregate, {sum, {1, seconds}}},
+                                                           {align,
+                                                            true}]),
+                            sets:from_list(L)
+                    end, 100, 300),
+    meck:unload().
 
 t_avg_aggregate(_Config) ->
     meck:new(freya_reader, [passthrough]),
@@ -284,16 +289,16 @@ t_avg_aggregate(_Config) ->
     ok = freya_writer:save(Publisher, DPIn2),
     ok = freya_writer:save(Publisher, DPIn3),
     ok = freya_writer:save(Publisher, DPIn4),
-    timer:sleep(2500),
     DPOut1 = DPIn1#data_point{value=1.0},
     DPOut2 = DPIn3#data_point{value=1.0},
-    {ok, [DPOut1, DPOut2]} =
-    freya_reader:search(?CS_READ_POOL,
-                        [{metric_name, MetricName},
-                         {start_time, Ts},
-                         {aggregate, {avg, {1, seconds}}}]),
-    meck:unload(),
-    ok.
+    ?th:keep_trying({ok, [DPOut1, DPOut2]},
+                    fun() ->
+                            freya_reader:search(?CS_READ_POOL,
+                                                [{metric_name, MetricName},
+                                                 {start_time, Ts},
+                                                 {aggregate, {avg, {1, seconds}}}])
+                    end, 100, 200),
+    meck:unload().
 
 t_avg_aggregate_aligned(_Config) ->
     meck:new(freya_reader, [passthrough]),
@@ -309,20 +314,32 @@ t_avg_aggregate_aligned(_Config) ->
     ok = freya_writer:save(Publisher, DPIn2),
     ok = freya_writer:save(Publisher, DPIn3),
     ok = freya_writer:save(Publisher, DPIn4),
-    timer:sleep(2500),
     DPOut1 = DPIn1#data_point{ts=Ts, value=1.0},
     DPOut2 = DPIn3#data_point{ts=Ts+1000, value=1.0},
-    {ok, [DPOut1, DPOut2]} =
-    freya_reader:search(?CS_READ_POOL,
-                        [{metric_name, MetricName},
-                         {start_time, Ts},
-                         {aggregate, {avg, {1, seconds}}},
-                         {align, true}]),
-    meck:unload(),
-    ok.
+    ?th:keep_trying({ok, [DPOut1, DPOut2]},
+                    fun() ->
+                            freya_reader:search(?CS_READ_POOL,
+                                                [{metric_name, MetricName},
+                                                 {start_time, Ts},
+                                                 {aggregate, {avg, {1, seconds}}},
+                                                 {align, true}])
+                    end, 100, 200),
+    meck:unload().
 
 t_tcp_version(_Config) ->
     {ok, Client} = freya_tcp_client:start_link(),
     {ok, Vsn} = freya:version(),
     {ok, [[{<<"version">>, Vsn}]]} = freya_tcp_client:version(Client),
-    {ok, [[{<<"version">>, Vsn}]]} = freya_tcp_client:version(Client).
+    {ok, [[{<<"version">>, Vsn}]]} = freya_tcp_client:version(Client),
+    ok = freya_tcp_client:stop(Client).
+
+t_tcp_write(_Config) ->
+    {ok, Client} = freya_tcp_client:start_link(),
+    MetricName = ?th:randomize(<<"via_tcp">>),
+    Ts = tic:now_to_epoch_msecs(),
+    ok = freya_tcp_client:put_metric(Client, MetricName, Ts, 666.66),
+    DPIn = freya_data_point:new(MetricName, Ts, <<"kairos_double">>, 666.66),
+    ?th:keep_trying({ok, [DPIn]}, fun() -> freya_reader:search(?CS_READ_POOL,
+                                                 [{metric_name, MetricName},
+                                                  {start_time, Ts}])
+                    end, 100, 200).
